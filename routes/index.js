564a5d2480b2f41b8855df3ae9a5ab5cf5a73009
David Ferrer <cursos@davidf.es>
2015-08-05 00:48:53 +0200

    Creacion login

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,20 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: []});
 });
 
- 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+
+// Definición de rutas de sesion
+router.get('/login',  sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
@@ -22,10 +27,12 @@ router.post('/quizes/create',              quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
-+router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
-+router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
+
+// Definición de rutas de comentarios
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
+
 
 router.get('/author',quizController.author);
-
 
 module.exports = router;
